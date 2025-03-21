2d2de2cfc45deff0fe99a90159e44fee3c51b095
elpaster <elpa3489@colorado.edu>
2025-03-21 12:40:36 -0600

    Created basic html for login page and added routes for login in index file

---
+++ ProjectSourceCode/src/index.js
@@ -0,0 +1,127 @@
+// *****************************************************
+// <!-- Section 1 : Import Dependencies -->
+// *****************************************************
+
+const express = require('express'); // To build an application server or API
+const app = express();
+const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
+const path = require('path');
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcryptjs'); //  To hash passwords
+const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+
+// *****************************************************
+// <!-- Section 2 : Connect to DB -->
+// *****************************************************
+
+// create `ExpressHandlebars` instance and configure the layouts and partials dir.
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
+});
+
+// database configuration
+const dbConfig = {
+    host: 'db', // the database server
+  port: 5432, // the database port
+  database: process.env.POSTGRES_DB, // the database name
+  user: process.env.POSTGRES_USER, // the user account to connect with
+  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+
+const db = pgp(dbConfig);
+
+// test your database
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
+
+// *****************************************************
+// <!-- Section 3 : App Settings -->
+// *****************************************************
+
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+
+// initialize session variables
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      saveUninitialized: false,
+      resave: false,
+    })
+  );
+  
+app.use(
+    bodyParser.urlencoded({
+    extended: true,
+    })
+);
+  
+// *****************************************************
+// <!-- Section 4 : API Routes -->
+// *****************************************************
+
+const auth = (req, res, next) => {
+    if (!req.session.user) {
+        //Default to login page.
+        return res.redirect('/login');
+    }
+    next(); 
+};
+
+app.get('/', (req, res) => {
+    res.redirect('/login'); 
+});
+  
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
+
+app.post('/login', async (req, res) => {
+    
+    try {
+        const { Username, Password } = req.body;
+        
+        const result = await db.query('SELECT * FROM users WHERE Username = $1', [req.body.Username]);
+        
+        if (result.length === 0) {
+            return res.render('pages/register', {
+                message: 'Username not found.',
+            });
+        }
+
+        const user = result[0]; 
+        console.log('User from DB:', user); 
+        console.log('Entered password:', Password);
+
+        const match = await bcrypt.compare(req.body.Password, user.password);
+
+        if (!match) {
+            return res.render('pages/login', {
+                message: 'Incorrect username or password.',
+            });
+        }
+
+        req.session.user = user;
+        req.session.save();
+        res.redirect('discover'); 
+    } catch (err) {
+        res.render('pages/login', { message: 'Error during login. Please try again.' });
+    }
+});
+
+
