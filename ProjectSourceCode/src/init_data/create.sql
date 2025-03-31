b9893366569e7b3cb6d555b2e25ef9f0958e900b
Lizzie Ruff <lizzie.ruff@colorado.edu>
2025-03-31 13:23:44 -0600

    Fix SQL syntax errors, and tested and implemented register

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,57 +1,57 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS comments;
-// Use DBML to define your database structure
-// Docs: https://dbml.dbdiagram.io/docs
+--Use DBML to define your database structure
+--Docs: https://dbml.dbdiagram.io/docs
 
-CREATE Table friends {
-  following_user_id INT NOT NULL
-  followed_user_id INT NOT NULL
+CREATE Table friends (
+  following_user_id INT NOT NULL,
+  followed_user_id INT NOT NULL,
   friends_since INT
-}
+);
 
-CREATE Table users {
-  id INT SERIAL PRIMARY KEY
-  username varchar(50) UNIQUE
-  password varchar(50) NOT NULL
-  email varchar(200)
-  profile_icon varchar(100)
-  bio varchar(150)
+CREATE Table users (
+  id SERIAL PRIMARY KEY,
+  username varchar(50) UNIQUE,
+  password varchar(255) NOT NULL,
+  email varchar(200),
+  profile_icon varchar(100),
+  bio varchar(150),
   created_at timestamp
-}
+);
 
-CREATE Table movies {
-  id INT SERIAL PRIMARY KEY
-  name varchar(100)
-  rating decimal
+CREATE Table movies (
+  id SERIAL PRIMARY KEY,
+  name varchar(100),
+  rating decimal,
   genre varchar(50)
-}
+);
 
-CREATE Table comments {
-  id INT SERIAL PRIMARY KEY
-  username varchar(50)
-  comment varchar(200)
+CREATE Table comments (
+  id SERIAL PRIMARY KEY,
+  username varchar(50),
+  comment varchar(200),
   rating decimal
-}
+);
 
-CREATE Table movies_to_comments {
-  movie_id integer NOT NULL
+CREATE Table movies_to_comments (
+  movie_id integer NOT NULL,
   comment_id integer NOT NULL
-}
+);
 
-CREATE Table movies_to_users {
-  user_id integer NOT NULL
-  movie_id integer NOT NULL
+CREATE Table movies_to_users (
+  user_id integer NOT NULL,
+  movie_id integer NOT NULL,
   status varchar(50)
-}
+);
 
-CREATE Table posts {
-  id INT SERIAL PRIMARY KEY
-  title varchar
-  body text 
-  user_id integer NOT NULL
-  status varchar
-  liked varchar
+CREATE Table posts (
+  id SERIAL PRIMARY KEY,
+  title varchar (100),
+  body text, 
+  user_id integer NOT NULL,
+  status varchar (50),
+  liked varchar (50),
   created_at timestamp
-}
+);
 
