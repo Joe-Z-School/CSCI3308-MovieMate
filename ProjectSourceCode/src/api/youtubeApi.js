e23e6a275f9c1d081eb6c5ff843ade1f127f3776
Conner Groth <connergroth@gmail.com>
2025-04-11 16:47:17 -0600

    added cache duration to youtubeApi.js

--- ProjectSourceCode/src/api/youtubeApi.js
+++ ProjectSourceCode/src/api/youtubeApi.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const OMDB_API_KEY = process.env.OMDB_API_KEY;
 
+// Cache for trailers
 const trailerCache = {};
 const CACHE_DURATION = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 
